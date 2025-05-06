a4e9e27b07fd3ba3276075a61591359eac6833a6
clean-423 <dariusjjones520@gmail.com>
2025-05-06 18:59:25 -0500

    added:connect to node

--- mempool_monitor_bot.py
+++ mempool_monitor_bot.py
@@ -1,3 +1,18 @@
 from web3 import Web3
+import os
+from dotenv import load_dotenv
 
-w3 = Web3(Web3.HTTPProvider("http"))
+#Load env 
+load_dotenv()
+api_key = os.getenv("INFURA_API_KEY")
+endpoint = os.getenv("INFURA_ENDPOINT")
+
+#Connect to Ethereum Node
+w3=Web3(Web3.HTTPProvider(endpoint))
+assert w3.is_connected()
+
+#Subscribe to Pending Transactions
+
+print(w3.is_connected())
+
+# w3 = Web3(Web3.HTTPProvider())
