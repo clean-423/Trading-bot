da4b83b4cdb7ad8d72ab5231289aba14fd776d1e
clean-423 <dariusjjones520@gmail.com>
2025-05-06 19:54:30 -0500

    added:mempool confirmation block scan

--- mempool_monitor_bot.py
+++ mempool_monitor_bot.py
@@ -5,14 +5,24 @@ from dotenv import load_dotenv
 #Load env 
 load_dotenv()
 api_key = os.getenv("INFURA_API_KEY")
-endpoint = os.getenv("INFURA_ENDPOINT")
+endpoint = os.getenv("CHAINSTACK_ENDPOINT")
 
 #Connect to Ethereum Node
 w3=Web3(Web3.HTTPProvider(endpoint))
-assert w3.is_connected()
 
-#Subscribe to Pending Transactions
+def monitor_mempool():
+    while True:
+        current_block = w3.eth.block_number
+        confirmed_block_number = current_block-1
+        confirmed_block = w3.eth.get_block(confirmed_block_number, full_transactions=True)
+        
+        for i, tx in enumerate(confirmed_block['transactions']):
+            print(f"{i+1}: 'From: ' {tx['from']} 'To:' {tx['to']}")
+        
+        print('\n')
+        # pending_block=w3.eth.get_block('pending')
+        # pending_transactions=pending_block['transactions']
+        # print(f"Current block: {current_block}. Pending transactions: {len(pending_transactions)}")
 
-print(w3.is_connected())
-
+monitor_mempool()
 # w3 = Web3(Web3.HTTPProvider())
